2debd0b243236e95cb3f686e5d546e5c7d3192f2
davidcbc <davidcbc@gmail.com>
2016-02-15 10:18:26 -0500

    Fixed coding style
    Changed coding style for functions to match recommendations for Python.
    Started new to_string method that still needs some adjustments

--- BinaryTree.py
+++ BinaryTree.py
@@ -4,9 +4,9 @@
 
 # Node class for BinaryTree
 class BinaryNode:
-    leftNode = None
-    rightNode = None
-    parentNode = None
+    left_node = None
+    right_node = None
+    parent_node = None
     value = None
 
     # Recursively add newValue to tree
@@ -15,30 +15,30 @@ class BinaryNode:
             self.value = newValue
             return
         if newValue < self.value:
-            if self.leftNode is None:
-                self.leftNode = BinaryNode()
-                self.leftNode.parentNode = self
-            self.leftNode.add(newValue)
+            if self.left_node is None:
+                self.left_node = BinaryNode()
+                self.left_node.parent_node = self
+            self.left_node.add(newValue)
         else:
-            if self.rightNode is None:
-                self.rightNode = BinaryNode()
-                self.rightNode.parentNode = self
-            self.rightNode.add(newValue)
+            if self.right_node is None:
+                self.right_node = BinaryNode()
+                self.right_node.parent_node = self
+            self.right_node.add(newValue)
 
     # Determine depth of tree recursively
     def depth(self):
         if self.value is None:
             return 0
         else:
-            if self.leftNode is None:
+            if self.left_node is None:
                 leftDepth = 1
             else:
-                leftDepth = self.leftNode.depth()+1
+                leftDepth = self.left_node.depth()+1
 
-            if self.rightNode is None:
+            if self.right_node is None:
                 rightDepth = 1
             else:
-                rightDepth = self.rightNode.depth()+1
+                rightDepth = self.right_node.depth()+1
             return max(leftDepth, rightDepth)
 
     # Find value in tree recursively.
@@ -47,24 +47,24 @@ class BinaryNode:
         if self.value is value:
             return True
         else:
-            if value < self.value and self.leftNode is not None:
-                return self.leftNode.find(value)
-            elif self.rightNode is not None:
-                return self.rightNode.find(value)
+            if value < self.value and self.left_node is not None:
+                return self.left_node.find(value)
+            elif self.right_node is not None:
+                return self.right_node.find(value)
             else:
                 return False
 
     # Determine the minimum value starting at this node recursively
-    def findMinimum(self):
-        if self.leftNode is None:
+    def find_minimum(self):
+        if self.left_node is None:
             return self.value
-        return self.leftNode.findMinimum()
+        return self.left_node.find_minimum()
 
     # Determine the maximum value starting at this node recursively
-    def findMaximum(self):
-        if self.rightNode is None:
+    def find_maximum(self):
+        if self.right_node is None:
             return self.value
-        return self.rightNode.findMaximum()
+        return self.right_node.find_maximum()
 
     # Delete value from the tree recursively
     # Return True if the value was in the tree
@@ -72,68 +72,98 @@ class BinaryNode:
     def delete(self, value):
         if self.value is value:
             print "Found " + str(value)
-            if self.leftNode is None and self.rightNode is None:
-                if self.parentNode is not None and self.parentNode.value >= value:
-                    self.parentNode.leftNode = None
-                elif self.parentNode is not None:
-                    self.parentNode.rightNode = None
+            if self.left_node is None and self.right_node is None:
+                if self.parent_node is not None and self.parent_node.value >= value:
+                    self.parent_node.left_node = None
+                elif self.parent_node is not None:
+                    self.parent_node.right_node = None
                 self.value = None
                 self = None
                 return True
-            elif self.leftNode is not None and self.rightNode is None:
-                self.value = self.leftNode.findMaximum()
-                return self.leftNode.delete(self.value)
+            elif self.left_node is not None and self.right_node is None:
+                self.value = self.left_node.find_maximum()
+                return self.left_node.delete(self.value)
             else:
-                self.value = self.rightNode.findMinimum()
-                return self.rightNode.delete(self.value)       
+                self.value = self.right_node.find_minimum()
+                return self.right_node.delete(self.value)       
         else:
             if self.value is None:
                 return False
             else:
-                if self.value >= value and self.leftNode is not None:
-                    return self.leftNode.delete(value)
-                elif self.value < value and self.rightNode is not None:
-                    return self.rightNode.delete(value)
+                if self.value >= value and self.left_node is not None:
+                    return self.left_node.delete(value)
+                elif self.value < value and self.right_node is not None:
+                    return self.right_node.delete(value)
                 else:
                     return False
 
     # Print the value of the node and it's children nodes
-    def toString(self):
-        toString = "Value: " + str(self.value)
-        if self.leftNode is not None:
-            toString += "\nLeft node->" + self.leftNode.toString()
-        if self.rightNode is not None:
-            toString += "\nRight node->" + self.rightNode.toString()
-        return toString
+    def to_string(self):
+        to_string = "Value: " + str(self.value)
+        if self.left_node is not None:
+            to_string += "\nLeft node->" + self.left_node.to_string()
+        if self.right_node is not None:
+            to_string += "\nRight node->" + self.right_node.to_string()
+        return to_string
 
+    def make_string(self, rows, depth, maxDepth):
+        rows[depth] += ("x"*(pow(maxDepth-depth,2)-len(str(self.value)))) + str(self.value)
+        if self.left_node is not None:
+            self.left_node.make_string(rows, depth+1, maxDepth)
+        else:
+            currDepth = depth+1
+            while currDepth < maxDepth:
+                rows[currDepth] += ("x"*(pow(maxDepth-currDepth,2)-1)) + "N"
+                currDepth += 1
+        if self.right_node is not None:
+            self.right_node.make_string(rows, depth+1, maxDepth)
+        else:
+            currDepth = depth+1
+            while currDepth < maxDepth:
+                rows[currDepth] += ("x"*(pow(maxDepth-currDepth,2)-1)) + "N"
+                currDepth += 1
+        
 # Main implementation
 class BinaryTree:
-    headNode = None
+    head_node = None
 
     # Add a new value to the tree
     def add(self, newValue):
-        if self.headNode is None:
-            self.headNode = BinaryNode()
-        self.headNode.add(newValue)
+        if self.head_node is None:
+            self.head_node = BinaryNode()
+        self.head_node.add(newValue)
 
     # Determine the depth of the tree
     def depth(self):
-        if self.headNode is None or self.headNode.value is None:
+        if self.head_node is None or self.head_node.value is None:
             return 0
         else:
-            return self.headNode.depth()
+            return self.head_node.depth()
     # Find a value in the tree
     # Return True if found and False if not found
     def find(self, value):
-        if self.headNode is not None:
-            return self.headNode.find(value)
+        if self.head_node is not None:
+            return self.head_node.find(value)
         else:
             return False
 
     # Delete a value from the tree
     # Return True if value is found and deleted, False if not found
     def delete(self, value):
-        if self.headNode is not None:
-            return self.headNode.delete(value)
+        if self.head_node is not None:
+            return self.head_node.delete(value)
         else:
             return False
+
+    def to_string(self):
+        depth = self.depth()
+        rows = []
+        retVal = ""
+        for i in range(0,depth):
+            rows.append("")
+        if self.head_node is not None:
+            self.head_node.make_string(rows, 0, depth)
+        for i in range(0, depth):
+            retVal += rows[i] + "\n"
+        return retVal
+        
--- BinaryTreeTests.py
+++ BinaryTreeTests.py
@@ -3,24 +3,24 @@ from BinaryTree import BinaryNode, BinaryTree
 ################################################################################
 # Setup functions for tests                                                    #
 ################################################################################
-def setupOneAdd():
+def setup_one_add():
     x = BinaryTree()
     x.add(10)
     return x
 
-def setupTwoAddsLeft():
+def setup_two_adds_left():
     x = BinaryTree()
     x.add(10)
     x.add(5)
     return x
 
-def setupTwoAddsRight():
+def setup_two_adds_right():
     x = BinaryTree()
     x.add(10)
     x.add(15)
     return x
 
-def setupMultipleAdds():
+def setup_multiple_adds():
     x = BinaryTree()
     x.add(10)
     x.add(5)
@@ -37,134 +37,134 @@ def setupMultipleAdds():
 ################################################################################
 
 ####                            Empty Tree tests                            ####
-# The headNode of a new tree is None
-def testEmptyTree():
+# The head_node of a new tree is None
+def test_empty_tree():
     x = BinaryTree()
-    assert x.headNode is None
+    assert x.head_node is None
 
-def testDeleteEmptyTreeFalse():
+def test_delete_empty_tree_false():
     x = BinaryTree()
     assert x.delete(1) is False
     
 ####                            One value tests                             ####
-def testOneAddHeadNotNone():
-    x = setupOneAdd()
-    assert x.headNode is not None
+def test_one_add_head_not_none():
+    x = setup_one_add()
+    assert x.head_node is not None
 
-def testOneAddHeadValue():
-    x = setupOneAdd()
-    assert x.headNode.value is 10
+def test_one_add_head_value():
+    x = setup_one_add()
+    assert x.head_node.value is 10
 
-def testOneAddHeadParent():
-    x = setupOneAdd()
-    assert x.headNode.parentNode is None
+def test_one_add_head_parent():
+    x = setup_one_add()
+    assert x.head_node.parent_node is None
 
-def testOneAddFind():
-    x = setupOneAdd()
+def test_one_add_find():
+    x = setup_one_add()
     assert x.find(10)
 
-def testOneAddDepth():
-    x = setupOneAdd()
+def test_one_add_depth():
+    x = setup_one_add()
     assert x.depth() is 1
 
-def testDeleteSingleEntryTrue():
-    x = setupOneAdd()
+def test_delete_single_entry_true():
+    x = setup_one_add()
     assert x.delete(10) is True
 
-def testDeleteSingleEntryFind():
-    x = setupOneAdd()
+def test_delete_single_entry_find():
+    x = setup_one_add()
     x.delete(10)
     assert x.find(10) is False
 
 ####                            Two value tests                             ####
-def testTwoAddsLeftParent():
-    x = setupTwoAddsLeft()
-    assert x.headNode.leftNode.parentNode is x.headNode
+def test_two_adds_left_parent():
+    x = setup_two_adds_left()
+    assert x.head_node.left_node.parent_node is x.head_node
     
-def testTwoAddsLeftFind():
-    x = setupTwoAddsLeft()
+def test_two_adds_left_find():
+    x = setup_two_adds_left()
     assert x.find(10)
     assert x.find(5)
     
-def testTwoAddsLeftDepth():
-    x = setupTwoAddsLeft()
+def test_two_adds_left_depth():
+    x = setup_two_adds_left()
     assert x.depth() is 2
 
-def testTwoAddsRightParent():
-    x = setupTwoAddsRight()
-    assert x.headNode.rightNode.parentNode is x.headNode
+def test_two_adds_right_parent():
+    x = setup_two_adds_right()
+    assert x.head_node.right_node.parent_node is x.head_node
 
-def testTwoAddsRightFind():
-    x = setupTwoAddsRight()
+def test_two_adds_right_find():
+    x = setup_two_adds_right()
     assert x.find(10)
     assert x.find(15)
 
-def testTwoAddsRightDepth():
-    x = setupTwoAddsRight()
+def test_two_adds_right_depth():
+    x = setup_two_adds_right()
     assert x.depth() is 2
 
-def testDeleteLeftLeafTrue():
-    x = setupTwoAddsLeft()
+def test_delete_left_leaf_true():
+    x = setup_two_adds_left()
     assert x.delete(5) is True
 
-def testDeleteLeftLeafFind():
-    x = setupTwoAddsLeft()
+def test_delete_left_leaf_find():
+    x = setup_two_adds_left()
     x.delete(5)
     assert x.find(5) is False
 
-def testDeleteLeftLeafNodeNone():
-    x = setupTwoAddsLeft()
+def test_delete_left_leaf_node_none():
+    x = setup_two_adds_left()
     x.delete(5)
-    assert x.headNode.leftNode is None
+    assert x.head_node.left_node is None
     
-def testDeleteRightLeafTrue():
-    x = setupTwoAddsRight()
+def test_delete_right_true():
+    x = setup_two_adds_right()
     assert x.delete(15) is True
 
-def testDeleteRightLeafFind():
-    x = setupTwoAddsRight()
+def test_delete_right_leaf_find():
+    x = setup_two_adds_right()
     x.delete(15)
     assert x.find(15) is False
 
-def testDeleteRightLeafNodeNone():
-    x = setupTwoAddsRight()
+def test_delete_right_leaf_node_none():
+    x = setup_two_adds_right()
     x.delete(15)
-    assert x.headNode.rightNode is None
+    assert x.head_node.right_node is None
 
-def testDeleteHeadLeftChildFind():
-    x = setupTwoAddsLeft()
+def test_delete_head_left_child_find():
+    x = setup_two_adds_left()
     x.delete(10)
     assert x.find(10) is False
 
-def testDeleteHeadLeftChildFindChildValue():
-    x = setupTwoAddsLeft()
+def test_delete_head_left_child_find_child_value():
+    x = setup_two_adds_left()
     x.delete(10)
     assert x.find(5) is True
 
-def testDeleteHeadLeftChildNone():
-    x = setupTwoAddsLeft()
+def test_delete_head_left_child_none():
+    x = setup_two_adds_left()
     x.delete(10)
-    assert x.headNode.leftNode is None
+    assert x.head_node.left_node is None
 
-def testDeleteHeadRightChildFind():
-    x = setupTwoAddsRight()
+def test_delete_head_right_child_find():
+    x = setup_two_adds_right()
     x.delete(10)
     assert x.find(10) is False
 
-def testDeleteHeadRightChildFindChildValue():
-    x = setupTwoAddsRight()
+def test_delete_head_right_child_find_child_value():
+    x = setup_two_adds_right()
     x.delete(10)
     assert x.find(15) is True
 
-def testDeleteHeadRightChildNone():
-    x = setupTwoAddsLeft()
+def test_delete_head_right_child_none():
+    x = setup_two_adds_left()
     x.delete(10)
-    assert x.headNode.rightNode is None
+    assert x.head_node.right_node is None
 
 
 ####                            Multiple value tests                        ####
-def testMultipleAddsFind():
-    x = setupMultipleAdds()
+def test_multiple_adds_find():
+    x = setup_multiple_adds()
     assert x.find(10)
     assert x.find(5)
     assert x.find(19)
@@ -174,97 +174,97 @@ def testMultipleAddsFind():
     assert x.find(18)
     assert x.find(23)
 
-def testMultipleAddsDepth():
-    x = setupMultipleAdds()
+def test_multiple_adds_depth():
+    x = setup_multiple_adds()
     assert x.depth() is 4
 
-def testMinimum():
-    x = setupMultipleAdds()
-    assert x.headNode.findMinimum() is 5
+def test_minimum():
+    x = setup_multiple_adds()
+    assert x.head_node.find_minimum() is 5
 
-def testMaximum():
-    x = setupMultipleAdds()
-    assert x.headNode.findMaximum() is 23
+def test_maximum():
+    x = setup_multiple_adds()
+    assert x.head_node.find_maximum() is 23
 
-def testDeleteHeadTwoChildrenFind():
-    x = setupMultipleAdds()
+def test_delete_head_two_children_find():
+    x = setup_multiple_adds()
     x.delete(10)
     assert x.find(10) is False
 
-def testDeleteHeadTwoChildrenFindChildValues():
-    x = setupMultipleAdds()
+def test_delete_head_two_children_find_child_values():
+    x = setup_multiple_adds()
     x.delete(10)
     assert x.find(5) is True
     assert x.find(16) is True
     assert x.find(23) is True
 
-def testDeleteHeadTwoChildrenHeadValue():
-    x = setupMultipleAdds()
+def test_delete_head_two_children_head_value():
+    x = setup_multiple_adds()
     x.delete(10)
-    assert x.headNode.value is 16
+    assert x.head_node.value is 16
     
-def testDeleteHeadTwoChildrenFindMissingChildValue():
-    x = setupMultipleAdds()
+def test_delete_head_two_children_find_missing_child_value():
+    x = setup_multiple_adds()
     x.delete(10)
-    assert x.headNode.rightNode.find(16) is False
+    assert x.head_node.right_node.find(16) is False
 
-def testDeleteNodeTwoChildrenFind():
-    x = setupMultipleAdds()
+def test_delete_node_two_children_find():
+    x = setup_multiple_adds()
     x.delete(19)
     assert x.find(19) is False
 
-def testDeleteNodeTwoChildrenValue():
-    x = setupMultipleAdds()
+def test_delete_node_two_children_value():
+    x = setup_multiple_adds()
     x.delete(19)
-    assert x.headNode.rightNode.value is 20
+    assert x.head_node.right_node.value is 20
 
-def testDeleteNodeTwoChildrenChildValue():
-    x = setupMultipleAdds()
+def test_delete_node_two_children_child_value():
+    x = setup_multiple_adds()
     x.delete(19)
-    assert x.headNode.rightNode.rightNode.value is 23
+    assert x.head_node.right_node.right_node.value is 23
 
 ####                            To String tests                             ####
-def testPrintSingleNode():
+def test_print_single_node():
     x = BinaryNode()
     x.value = 10
-    assert x.toString() == "Value: 10"
+    assert x.to_string() == "Value: 10"
 
-def testPrintTwoNodesLeft():
+def test_print_two_nodes_left():
     x = BinaryNode()
     x.value = 10
-    x.leftNode = BinaryNode()
-    x.leftNode.value = 5
-    print x.toString()
-    assert x.toString() == "Value: 10\nLeft node->Value: 5"
+    x.left_node = BinaryNode()
+    x.left_node.value = 5
+    print x.to_string()
+    assert x.to_string() == "Value: 10\nLeft node->Value: 5"
 
-def testPrintTwoNodesRight():
+def test_print_two_nodes_right():
     x = BinaryNode()
     x.value = 10
-    x.rightNode = BinaryNode()
-    x.rightNode.value = 15
-    print x.toString()
-    assert x.toString() == "Value: 10\nRight node->Value: 15"
+    x.right_node = BinaryNode()
+    x.right_node.value = 15
+    print x.to_string()
+    assert x.to_string() == "Value: 10\nRight node->Value: 15"
 
 
-def testPrintThreeNodesEven():
+def test_print_three_nodes_even():
     x = BinaryNode()
     x.value = 10
-    x.leftNode = BinaryNode()
-    x.leftNode.value = 5
-    x.rightNode = BinaryNode()
-    x.rightNode.value = 15
-    print x.toString()
-    assert x.toString() == "Value: 10\nLeft node->Value: 5\nRight node->Value: 15"
+    x.left_node = BinaryNode()
+    x.left_node.value = 5
+    x.right_node = BinaryNode()
+    x.right_node.value = 15
+    print x.to_string()
+    assert x.to_string() == "Value: 10\nLeft node->Value: 5\nRight node->Value: 15"
     
-def testPrintFourNodes():
+def test_print_four_nodes():
     x = BinaryNode()
     x.value = 10
-    x.leftNode = BinaryNode()
-    x.leftNode.value = 5
-    x.rightNode = BinaryNode()
-    x.rightNode.value = 15
-    x.leftNode.leftNode = BinaryNode()
-    x.leftNode.leftNode.value = 1
-    print x.toString()
-    assert x.toString() == "Value: 10\nLeft node->Value: 5\nLeft node->Value: 1\nRight node->Value: 15"
+    x.left_node = BinaryNode()
+    x.left_node.value = 5
+    x.right_node = BinaryNode()
+    x.right_node.value = 15
+    x.left_node.left_node = BinaryNode()
+    x.left_node.left_node.value = 1
+    print x.to_string()
+    assert x.to_string() == "Value: 10\nLeft node->Value: 5\nLeft node->Value: 1\nRight node->Value: 15"
 
