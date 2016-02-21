07120c6c8e766f75fe6cfd607f677bbee17413e7
davidcbc <davidcbc@gmail.com>
2016-02-21 18:38:18 -0500

    Added move and tests

--- AI/Search/Maze.py
+++ AI/Search/Maze.py
@@ -12,13 +12,39 @@ class Maze:
         self.END_COLOR = (255, 0, 0)
 
         # Load image.
+        self.file_in = maze
         self.image = Image.open(self.file_in)
         self.image = self.image.convert('RGB')
         self.pixels = self.image.load()
         self._determine_start_and_finish()
         self._clean_image()
-
+        self.current_position = self.START_POSITION
         logging.info("Loaded image '{0}' ({1} pixels).".format(self.file_in, self.image.size))
+
+    def move(self, direction):
+        x,y = self.current_position
+        if direction == "n":
+            if self.is_open(x,y-1) is True:
+                self.current_position = (x,y-1)
+                return True
+            return False
+        if direction == "s":
+            if self.is_open(x,y+1) is True:
+                self.current_position = (x,y+1)
+                return True
+            return False
+        if direction == "e":
+            if self.is_open(x+1,y) is True:
+                self.current_position = (x+1,y)
+                return True
+            return False
+        if direction == "w":
+            if self.is_open(x-1,y) is True:
+                self.current_position = (x-1,y)
+                return True
+            return False
+        return False
+
 
     def _determine_start_and_finish(self):
         x, y = self.image.size
@@ -45,12 +71,16 @@ class Maze:
 
 
     def is_open(self, x, y):
+        borderX, borderY = self.image.size
+        if x < 0 or y < 0 or x >= borderX or y >= borderY:
+            return False
         return True if self.pixels[x,y] == self.OPEN_COLOR else False
 
     def is_end(self, x, y):
-        logging.info("X, Y: " + str(x) + ", " + str(y))
-        logging.info("END: " + str(self.END_POSITION))
         return True if (x,y) == self.END_POSITION else False
+
+    def at_end(self):
+        return True if (self.current_position) == self.END_POSITION else False
 
     def to_string(self):
         x,y = self.image.size
--- AI/Search/MazeTest.py
+++ AI/Search/MazeTest.py
@@ -23,3 +23,60 @@ def test_maze_is_open_true():
 def test_maze_is_open_false():
     testMaze = Maze("testmaze.png")
     assert testMaze.is_open(0,1) is False
+
+def test_move_north():
+    testMaze = Maze("maze0.png")
+    assert testMaze.move('n')
+
+def test_move_south():
+    testMaze = Maze("maze0.png")
+    testMaze.current_position = (1,47)
+    assert testMaze.move('s')
+
+def test_move_east():
+    testMaze = Maze("maze0.png")
+    testMaze.current_position = (6,47)
+    assert testMaze.move('e')
+
+def test_move_west():
+    testMaze = Maze("maze0.png")
+    testMaze.current_position = (6,47)
+    assert testMaze.move('w')
+
+def test_move_north_fail():
+    testMaze = Maze("maze0.png")
+    testMaze.current_position = (0,49)
+    assert not testMaze.move('n')
+
+def test_move_south_fail():
+    testMaze = Maze("maze0.png")
+    testMaze.current_position = (0,48)
+    assert not testMaze.move('s')
+
+def test_move_east_fail():
+    testMaze = Maze("maze0.png")
+    assert not testMaze.move('e')
+
+def test_move_west_fail():
+    testMaze = Maze("maze0.png")
+    assert not testMaze.move('w')
+
+def test_move_north_fail_border():
+    testMaze = Maze("maze0.png")
+    testMaze.current_position = (49,49)
+    assert not testMaze.move('n')
+
+def test_move_south_fail_border():
+    testMaze = Maze("maze0.png")
+    testMaze.current_position = (0,0)
+    assert not testMaze.move('s')
+
+def test_move_east_fail_border():
+    testMaze = Maze("maze0.png")
+    testMaze.current_position = (49,49)
+    assert not testMaze.move('e')
+
+def test_move_west_fail_border():
+    testMaze = Maze("maze0.png")
+    testMaze.current_position = (0,0)
+    assert not testMaze.move('w')
