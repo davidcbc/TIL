3107270b9503e87c7fa2994337e586b443dd2a06
davidcbc <davidcbc@gmail.com>
2016-02-21 21:12:38 -0500

    Added Breadth First Search

--- AI/Search/BFSSolver.py
+++ AI/Search/BFSSolver.py
@@ -1,36 +1,42 @@
 from Maze import Maze
+from collections import deque
 import sys
 import logging
 
 
-class DFSSolver:
+class BFSSolver:
     def __init__(self,maze):
         self.maze = Maze(maze)
         self.current_position = self.maze.current_position
         self.visited_positions = {self.current_position}
-        self.position_stack = [self.current_position]
+        self.path_queue = deque()
 
     def solve(self):
         x,y = self.current_position
-        while(not self.maze.is_end(x,y)):
+        path = [self.current_position]
+        while not self.maze.is_end(x,y):
             self.visited_positions.add((x,y))
             if (x,y+1) not in self.visited_positions and self.maze.is_open(x,y+1):
-                self.position_stack.append((x,y+1))
-                y += 1
-            elif (x,y-1) not in self.visited_positions and self.maze.is_open(x,y-1):
-                self.position_stack.append((x,y-1))
-                y -= 1
-            elif (x+1,y) not in self.visited_positions and self.maze.is_open(x+1,y):
-                self.position_stack.append((x+1,y))
-                x += 1
-            elif (x-1,y) not in self.visited_positions and self.maze.is_open(x-1,y):
-                self.position_stack.append((x-1,y))
-                x -= 1
-            else:
-                prevX, prevY = self.position_stack.pop()
-                x,y = self.position_stack[len(self.position_stack)-1]
+                new_path = list(path)
+                new_path.append((x, y + 1))
+                self.path_queue.append(new_path)
+            if (x,y-1) not in self.visited_positions and self.maze.is_open(x,y-1):
+                new_path = list(path)
+                new_path.append((x, y - 1))
+                self.path_queue.append(new_path)
+            if (x+1,y) not in self.visited_positions and self.maze.is_open(x+1,y):
+                new_path = list(path)
+                new_path.append((x + 1, y))
+                self.path_queue.append(new_path)
+            if (x-1,y) not in self.visited_positions and self.maze.is_open(x-1,y):
+                new_path = list(path)
+                new_path.append((x - 1, y))
+                self.path_queue.append(new_path)
+            path = self.path_queue.popleft()
+            x,y = path[len(path)-1]
+
         prevX, prevY = self.current_position
-        for (solX, solY) in self.position_stack:
+        for (solX, solY) in path:
             if solY > prevY:
                 if not self.maze.move('s'):
                     logging.error("Failed move south")
@@ -46,10 +52,11 @@ class DFSSolver:
             prevX = solX
             prevY = solY
         if self.maze.at_end():
-            logging.info("Solved in " + str(len(self.position_stack)) + " moves!")
+            logging.info("Solved in " + str(len(path)) + " moves!")
         else:
             logging.error("Failed! End position: " + str(self.maze.END_POSITION) + " Current position: "  + str(self.maze.current_position))
 
+
 if __name__ == '__main__':
-    solver = DFSSolver(sys.argv[1])
+    solver = BFSSolver(sys.argv[1])
     solver.solve()
